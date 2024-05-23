958a5963a373392fe51cdc0bbc1c357b405484ea
Curtis Redgate <curtis@monad.com>
2024-05-23 15:48:48 -0400

    test

--- generate_docs.sh
+++ generate_docs.sh
@@ -45,6 +45,6 @@ fi
 gomarkdoc ./... --output ./docs/docs.md
 
 # Convert Markdown to HTML using pandoc
-pandoc ./docs/docs.md --toc --metadata title="Documentation" -c https://unpkg.com/sakura.css/css/sakura.css --standalone -o ./docs/index.html
+pandoc ./docs/docs.md --toc --metadata title="index" -c https://unpkg.com/sakura.css/css/sakura.css --standalone -o ./docs/index.html
 
 exit 0
