76d58af184a8a08fdef3075e540cd322b8d2534f
Curtis Redgate <curtis@monad.com>
2024-05-23 13:50:45 -0400

    test data

---
+++ people.go
@@ -0,0 +1,27 @@
+package test_docs
+
+// People is a struct that contains a name and an age
+type People struct {
+	Name string
+	Age  int
+}
+
+// GetName returns the name of the People
+func (p People) GetName() string {
+	return p.Name
+}
+
+// GetAge returns the age of the People
+func (p People) GetAge() int {
+	return p.Age
+}
+
+// SetName sets the name of the People
+func (p People) SetName(name string) {
+	p.Name = name
+}
+
+// SetAge sets the age of the People
+func (p People) SetAge(age int) {
+	p.Age = age
+}
